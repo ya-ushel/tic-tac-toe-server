a01097d9d7201b6c9987e88f3dd97541988ce1d0
Ilchenko Dmitriy <ilchenko.dmitriyg@gmail.com>
2021-10-20 22:20:56 +0300

    Connected server with client.

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const parentDirname = require("path").resolve(__dirname, "..");
 
 const server = http.Server(app).listen(8080);
 const { startLobbies } = require("./handlers/lobby");
 const { initFirebase } = require("../shared/firebase");
 
 const { createLobby } = require("./routes");
-console.log(createLobby);
+console.log(parentDirname);
 app.use(express.static(__dirname + "/../client/"));
 app.use(express.static(__dirname + "/../node_modules/"));
 
@@ -17,8 +18,8 @@ initFirebase();
 
 app.get("/", (req, res) => {
   // res.sendFile("index.html", { root: __dirname + "/../client" });
-  const stream = fs.createReadStream(__dirname + "/../client/index.html");
-  stream.pipe(res.query);
+
+  res.sendFile("/client/dist/index.html", { root: parentDirname });
 });
 
 app.get("/create-lobby", createLobby);
