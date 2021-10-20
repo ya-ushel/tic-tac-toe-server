6dd6e9e985559c4ff00e0405f84de52e3a02434a
Bogdan <bogdan.malina.s@gmail.com>
2021-10-20 21:59:33 +0300

    updated clien

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "App",
+
+  components: {},
+  computed: {
+    ...mapGetters(["getUser"]),
+  },
+  mounted() {
+    // !this.getUser ? this.$router.push("/") : this.$route.push("/auth");
+    console.log(this.$router, "getUser");
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
