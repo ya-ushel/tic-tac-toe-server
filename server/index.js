8ddc0952f40fc405ca4873f8577574424b96e8cb
Ilchenko Dmitriy <ilchenko.dmitriyg@gmail.com>
2021-10-21 23:40:15 +0300

    Added cors.

--- server/index.js
+++ server/index.js
@@ -2,16 +2,24 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 
 const port = process.env.PORT || 8080;
 const server = http.Server(app).listen(port);
 const { startLobbies } = require("./handlers/lobby");
 const { initFirebase } = require("../shared/firebase");
-
 const { createLobby, joinLobby, leaveLobby, getRooms } = require("./routes");
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(express.static(__dirname + "/../node_modules/"));
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 startLobbies(server);
 initFirebase();
